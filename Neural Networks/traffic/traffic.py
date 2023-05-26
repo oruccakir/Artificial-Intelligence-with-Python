41d0d230f4a198390254cb28b08c851c07bb5901
oruccakir <oruccakir2525@gmail.com>
2023-05-26 08:22:10 +0300

    Neural Networks project is done

--- Neural Networks/traffic/traffic.py
+++ Neural Networks/traffic/traffic.py
@@ -7,11 +7,11 @@ from PIL import Image
 
 from sklearn.model_selection import train_test_split
 
-EPOCHS = 10
+EPOCHS = 30
 IMG_WIDTH = 30
 IMG_HEIGHT = 30
 NUM_CATEGORIES = 5
-TEST_SIZE = 0.4
+TEST_SIZE = 0.5
 
 
 def main():
@@ -96,6 +96,33 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
+
+    model = tf.keras.models.Sequential(
+        [
+            tf.keras.layers.Conv2D(32, (3,3), activation="relu", input_shape = (IMG_WIDTH,IMG_HEIGHT,3)),
+
+            tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+
+            tf.keras.layers.Flatten(),
+
+            tf.keras.layers.Dense(128,activation="relu"),
+
+            tf.keras.layers.Dropout(0,5),
+
+            tf.keras.layers.Dense(NUM_CATEGORIES,activation="softmax")
+
+        ]
+    )
+
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
+
+
     raise NotImplementedError
 
 
